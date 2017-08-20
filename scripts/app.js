b9ce80c3c9308ec7ce0c94620faace340f9f2472
juan <juancamilogaviriaacosta@gmail.com>
2017-08-20 11:21:41 -0500

    service worker

--- scripts/app.js
+++ scripts/app.js
@@ -201,4 +201,13 @@
         ];
         app.saveSelectedTimetables();
     }
+
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+                .register('./service-worker.js')
+                .then(function () {
+                    console.log('Service Worker Registered');
+                });
+    }
 })();
